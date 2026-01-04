e4d8ca7125690d2aee86779b665340903855afc7
Jordan Cooper <jordan.cooper@oursoftwaregroup.com>
2026-01-03 18:25:05 -0800

    Copied from template

---
+++ app/main.py
@@ -0,0 +1,10 @@
+"""Main entry point for the application."""
+
+
+def main() -> None:
+    """Run the application."""
+    print("Hello from my-project!")
+
+
+if __name__ == "__main__":
+    main()
